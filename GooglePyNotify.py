e60f49d681e0957f94fa174b17d3abd917066450
Varre, Anil [WNPUS] <AVarre@ITS.JNJ.COM>
2018-11-10 21:07:51 -0500

    adding loging

--- GooglePyNotify.py
+++ GooglePyNotify.py
@@ -92,7 +92,7 @@ if not os.path.exists(MP3_CACHE_DIR):
 
 print("Getting chromecasts...")
 CHROMECASTS = pychromecast.get_chromecasts()
-print("chromecasts found: " + ' '.join([cc.device.model_name for cc in CHROMECASTS]))
+print("chromecasts found: " + '\n'.join([cc.device.model_name for cc in CHROMECASTS if cc.device.model_name == "Google Home" or cc.device.model_name == "Google Home Mini"]))
 
 print(time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, HOST_PORT))
 httpServer = HTTPServer((HOST_NAME, HOST_PORT), HttpServer) #HTTP Server Stuff (Python Librarys)
